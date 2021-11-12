fd230412a9e2ec283fc8657717b2f76113b84ace
LitMSCTBB <minecraftandcomputers@gmail.com>
2021-11-12 14:11:38 -0600

    try this ammmarr

--- src/285Z_Main/auxAutons.cpp
+++ src/285Z_Main/auxAutons.cpp
@@ -6,25 +6,25 @@
 #include "../include/285Z_Subsystems/pid.hpp"
 #include "../include/285z/initSensors.hpp"
 
-auto motion =
-    ChassisControllerBuilder()
-        .withMotors({frontLeftPort, backLeftPort}, {frontRightPort, backRightPort})
-        .withDimensions(AbstractMotor::gearset::green, scales)
-        .withMaxVelocity(200)
-        .build();
-auto profileController = AsyncMotionProfileControllerBuilder().withLimits({0.5, 2.0, 10.0}).withOutput(motion).buildMotionProfileController();
+// std::shared_ptr<okapi::ChassisController> motion =
+//     ChassisControllerBuilder()
+//         .withMotors({frontLeftPort, backLeftPort}, {frontRightPort, backRightPort})
+//         .withDimensions(AbstractMotor::gearset::green, scales)
+//         .withMaxVelocity(200)
+//         .build();
+// std::shared_ptr<okapi::AsyncMotionProfileController> profileController = AsyncMotionProfileControllerBuilder().withLimits({0.5, 2.0, 10.0}).withOutput(motion).buildMotionProfileController();
 
-void move(std::shared_ptr<okapi::AsyncMotionProfileController> profile, okapi::QLength distance, bool dir){
-  profile->generatePath({
-    {0_ft,0_ft,0_deg},
-    {distance, 0_ft,0_deg}},
-    "M"
-  );
+// void move(std::shared_ptr<okapi::AsyncMotionProfileController> profile, okapi::QLength distance, bool dir){
+//   profile->generatePath({
+//     {0_ft,0_ft,0_deg},
+//     {distance, 0_ft,0_deg}},
+//     "M"
+//   );
 
-  profile->setTarget("M", dir);
-  profile->waitUntilSettled();
-  profile->removePath("M");
-}
+//   profile->setTarget("M", dir);
+//   profile->waitUntilSettled();
+//   profile->removePath("M");
+// }
 
 //****************** SKILLS ***********************************//
 void skillsAuto() {
