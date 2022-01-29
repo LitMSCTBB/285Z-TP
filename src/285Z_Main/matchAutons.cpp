48f5444185a4bc4d58a7e1b9e7c72dca25d93b09
LitMSCTBB <minecraftandcomputers@gmail.com>
2022-01-29 12:03:57 -0600

    new redleftblueleft auto

--- src/285Z_Main/matchAutons.cpp
+++ src/285Z_Main/matchAutons.cpp
@@ -1,4 +1,5 @@
 #include "../include/285z/initRobot.hpp"
+#include "../include/285z/initSensors.hpp"
 #include "../include/285Z_Subsystems/lift.hpp"
 #include "../include/285z/functions.hpp"
 #include "../include/285Z_Aux/gui.hpp"
@@ -60,4 +61,32 @@ void winPoint(std::shared_ptr<okapi::AsyncMotionProfileController> slow,
 
 
 
+}
+
+void newRedLeftBlueLeft(std::shared_ptr<okapi::AsyncMotionProfileController> slow,
+                     std::shared_ptr<okapi::AsyncMotionProfileController> med,
+                     std::shared_ptr<okapi::AsyncMotionProfileController> fast)
+{
+  /*
+  chassisaut->turnAngle(-20_deg);
+  twoBarController->setTarget(-3100);
+  pros::delay(1500);
+  move(profileController, 0.4_ft, bwd);
+  pros::delay(1500);
+  twoBarController->setTarget(-1495);
+  pros::delay(1500);
+  move(profileController, 0.35_ft, fwd);
+  pros::delay(1500);
+  intakeMotor.moveVoltage(9500);
+  */
+  move(fast, 13.5_ft, fwd);
+  move(med, 2.4_ft, fwd);
+  clawPiston.set_value(true);
+  turn(-12);
+  move(fast, 13.5_ft, bwd);
+  
+  intakeMotor.moveVelocity(0);
+  turn(90);
+  turn(180);
+  turn(0);
 }
